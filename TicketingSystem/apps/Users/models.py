badff0a2bb2bb6f658389187e3cc059a610d3014
Mohammad Homanloo <homanloom@gmail.com>
2025-11-04 16:08:15 +0330

    Add tickets models with admin integration
    In addition, a temporary and small Order model is implemented for testing and resolving import error. The finalized Order model shall be developed on later stage.

--- TicketingSystem/apps/Users/models.py
+++ TicketingSystem/apps/Users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Order(models.Model):
+    """
+    A model for a users orders.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    order_number = models.CharField(max_length=255)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
